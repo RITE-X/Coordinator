770be7042d8d3c9a6b5c130085ee7801aa9decb1
RITE-X <re.ritex@gmail.com>
2022-02-28 01:27:21 +0200

    Add: Add bot greeting and group support

--- Coordinator/Program.cs
+++ Coordinator/Program.cs
@@ -34,49 +34,80 @@ while (true)
 
 async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
 {
-    if (update.Type != UpdateType.Message)
-        return;
-
-
-    var chatId = update.Message.Chat.Id;
-    var message = update.Message;
-
-    if (message.Text == "/start")
+    try
     {
-    }
-
-    if (chatId == BotSettingsInit.AdminChatId)
-    {
-        if (message.ReplyToMessage is null)
+        if (update.Type != UpdateType.Message)
             return;
-        
-        if (message.ReplyToMessage.ForwardFrom is null)
+
+
+        var chatId = update.Message.Chat.Id;
+        var message = update.Message;
+
+        if (message.Text == "/start")
         {
-            await botClient.SendTextMessageAsync(BotSettingsInit.AdminChatId, "Ви не можете відповісти на повідомлення користувача з прихованим профілем",
-                cancellationToken: cancellationToken);
+            await botClient.SendTextMessageAsync(chatId,
+                "Солдат из Беларуси и России! Это не твоя война. Ты не выбирал эту войну и агрессию. " +
+                "Мы знаем что тебя доставили на Украинскую землю силой и обманом. Мы не хотим тебя убивать." +
+                " Бросай оружие и сохрани себе жизнь!");
+
+            await botClient.SendTextMessageAsync(chatId,
+                "Что делать если против воли насильно попал на территорию Украины?!\n\n • Избегай участия в боевых действиях\n • Не стреляй\n • Сложи оружие и обратись к военнослужащим Украинской Армии\n • Отправь сюда фото с геолокацией\n • С поднятыми руками иди по направлению к ближайшему населенному пункту");
+            
+            
+
+            // await botClient.SendTextMessageAsync(chatId, "https://t.me/surrender_and_survive");
             return;
         }
 
+        if (chatId == BotSettingsInit.AdminChatId)
+        {
+            if (message.ReplyToMessage is null)
+            {
+                await botClient.SendTextMessageAsync(BotSettingsInit.AdminChatId,
+                    "Используйте кнопку ответить, для отправки сообщений конкретным пользователям",
+                    cancellationToken: cancellationToken);
+                return;
+            }
+                
+            if (message.ReplyToMessage.ForwardFrom is null)
+            {
+                await botClient.SendTextMessageAsync(BotSettingsInit.AdminChatId,
+                    "Вы не можете ответить на сообщение пользователя со скрытым профилем",
+                    cancellationToken: cancellationToken);
+                return;
+            }
 
-        var replyToMessage = message.ReplyToMessage;
-        await botClient.SendTextMessageAsync(replyToMessage.ForwardFrom.Id, message.Text, cancellationToken: cancellationToken);
+
+            var replyToMessage = message.ReplyToMessage;
+            await botClient.SendTextMessageAsync(replyToMessage.ForwardFrom.Id, message.Text,
+                cancellationToken: cancellationToken);
+        }
+        else
+        {
+            if (message.Chat.Type != ChatType.Private)
+                return;
+
+            await botClient.ForwardMessageAsync(BotSettingsInit.AdminChatId, chatId, message.MessageId,
+                cancellationToken: cancellationToken);
+            await botClient.SendTextMessageAsync(chatId, "Ваше сообщение доставлено, ожидайте ответа координатора",
+                cancellationToken: cancellationToken);
+        }
     }
-    else
+    catch (Exception e)
     {
-        await botClient.ForwardMessageAsync(BotSettingsInit.AdminChatId, chatId, message.MessageId,
-            cancellationToken: cancellationToken);
+        Console.WriteLine(e);
     }
 }
 
 Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
 {
-    var ErrorMessage = exception switch
+    var errorMessage = exception switch
     {
         ApiRequestException apiRequestException
             => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
         _ => exception.ToString()
     };
 
-    Console.WriteLine(ErrorMessage);
+    Console.WriteLine(errorMessage);
     return Task.CompletedTask;
 }
